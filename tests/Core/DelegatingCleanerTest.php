d26e45e610b7a0d850a40995e11fb82cbe8480aa
Cliff Odijk <cliff@obro.nl>
2015-05-25 00:22:57 +0200

    Added tests for the Core DelegatingCleaner

---
+++ tests/Core/DelegatingCleanerTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace LongRunning\Tests\Core;
+
+use LongRunning\Core\Cleaner;
+use LongRunning\Core\DelegatingCleaner;
+
+class DelegatingCleanerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function it_tests_the_delecated_cleaners()
+    {
+        $cleaner = new DelegatingCleaner([
+            $this->getCleaner(),
+            $this->getCleaner(),
+            $this->getCleaner(),
+        ]);
+        $cleaner->cleanUp();
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject|Cleaner
+     */
+    private function getCleaner()
+    {
+        $cleaner = $this->getMock('LongRunning\Core\Cleaner');
+        $cleaner
+            ->expects($this->once())
+            ->method('cleanUp');
+
+        return $cleaner;
+    }
+}
