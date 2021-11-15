cde9e0d4396e72ac6ebe2eda2671f1927c074677
Ruud Kamphuis <ruudk@users.noreply.github.com>
2021-11-15 09:11:53 +0100

    Convert monorepo-builder config from YAML to PHP

---
+++ monorepo-builder.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\ComposerJsonManipulator\ValueObject\ComposerJsonSection;
+use Symplify\MonorepoBuilder\ValueObject\Option;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $parameters = $containerConfigurator->parameters();
+
+    $parameters->set(Option::DEFAULT_BRANCH_NAME, 'main');
+    $parameters->set(Option::DATA_TO_APPEND, [
+        ComposerJsonSection::REQUIRE_DEV => [
+            'phpunit/phpunit' => '^8.5.12',
+            'symplify/monorepo-builder' => '^9.0',
+        ],
+    ]);
+    $parameters->set(Option::PACKAGE_DIRECTORIES, [__DIR__ . '/packages']);
+};
